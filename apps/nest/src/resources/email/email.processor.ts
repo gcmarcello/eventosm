970610ddba7c1013aacb389508fe6d5dd06f503b
gcmarcello <marcellogcoelho@outlook.com>
2024-03-01 09:53:47 +0000

    renamed real backend folder

--- apps/nest/src/resources/email/email.processor.ts
+++ apps/nest/src/resources/email/email.processor.ts
@@ -9,18 +9,25 @@ import { Job, Queue } from "bull";
 import { SendgridMailService } from "./sg";
 
 import { MailDataRequired } from "@sendgrid/mail";
+import { SettingsService } from "../settings/settings.service";
 
 @Processor("email")
 export class EmailProcessor {
   constructor(
     private sgMail: SendgridMailService,
-    @InjectQueue("email") private emailQueue: Queue
+    @InjectQueue("email") private emailQueue: Queue,
+    private readonly settingsService: SettingsService
   ) {}
 
   @Process()
   async sendEmail(job: Job<MailDataRequired>) {
     return await this.sgMail
-      .send({ ...job.data, mailSettings: { sandboxMode: { enable: false } } })
+      .send({
+        ...job.data,
+        mailSettings: {
+          sandboxMode: { enable: this.settingsService.isDevelopment },
+        },
+      })
       .catch((error) => console.log(error));
   }
 
