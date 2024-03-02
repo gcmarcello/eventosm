14b30882d8008e8b541178381c672c2c558b9260
gcmarcello <marcellogcoelho@outlook.com>
2024-03-02 00:39:47 +0000

    Update .kanban file and remove console.log statements

--- apps/dash/src/app/api/emails/service.ts
+++ apps/dash/src/app/api/emails/service.ts
@@ -2,7 +2,6 @@ import { getServerEnv } from "@/app/api/env";
 import { Email, EmailTemplate } from "email-templates";
 
 export async function sendEmail<T extends EmailTemplate>(arg: Email<T>[]) {
-  console.log(arg, getServerEnv("QUEUE_URL"));
   return await fetch(getServerEnv("QUEUE_URL") + "/email", {
     method: "POST",
     headers: {
