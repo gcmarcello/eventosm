560c1f1ddc10699186a8e7a6833bd5173a7e5995
gcmarcello <marcellogcoelho@outlook.com>
2024-03-03 18:03:39 +0000

    Seed changes

--- apps/dash/prisma/seed/user.seed.ts
+++ apps/dash/prisma/seed/user.seed.ts
@@ -3,13 +3,12 @@ import { hashInfo } from "../../src/utils/bCrypt";
 import { prisma } from "../prisma";
 
 export const userSeed = async () => {
-  return await prisma.user.upsert({
-    where: { email: "sergio@loroza.com" },
-    create: {
+  return await prisma.user.create({
+    data: {
       document: "88325584084",
       email: "sergio@loroza.com",
       fullName: "Sergio Loroza",
-      role: "user",
+      role: "admin",
       password: await hashInfo("123456"),
       phone: "21999999999",
       info: {
@@ -18,25 +17,6 @@ export const userSeed = async () => {
           number: "123",
           zipCode: "22220000",
           birthDate: dayjs("1980-01-01").toDate(),
-          cityId: "3304557",
-          stateId: "33",
-          gender: "male",
-        },
-      },
-    },
-    update: {
-      document: "88325584084",
-      email: "sergio@loroza.com",
-      fullName: "Sergio Loroza",
-      role: "user",
-      password: await hashInfo("123456"),
-      phone: "21999999999",
-      info: {
-        create: {
-          address: "Rua do Catete",
-          number: "123",
-          zipCode: "22220000",
-          birthDate: dayjs("1980-01-01").toISOString(),
           cityId: "3304557",
           stateId: "33",
           gender: "male",
