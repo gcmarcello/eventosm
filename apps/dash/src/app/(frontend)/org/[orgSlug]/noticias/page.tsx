43a36a2f854172c91cc3f6618bb51a94200ec0b1
gcmarcello <marcellogcoelho@outlook.com>
2024-03-18 21:03:32 +0000

    Update noticias layout and page components

--- apps/dash/src/app/(frontend)/org/[orgSlug]/noticias/page.tsx
+++ apps/dash/src/app/(frontend)/org/[orgSlug]/noticias/page.tsx
@@ -1,3 +1,70 @@
-export default function NewsPage({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+import { Title } from "odinkit";
+import NewsCard from "../../_shared/NewsCard";
+import clsx from "clsx";
+import { Menu, Transition } from "@headlessui/react";
+import { EllipsisVerticalIcon } from "@heroicons/react/24/outline";
+import { Fragment } from "react";
+import { Date } from "odinkit/client";
+import Image from "next/image";
+
+export default async function NewsPage({
+  params,
+}: {
+  params: { orgSlug: string };
+}) {
+  const news = await prisma.news.findMany({
+    where: { Organization: { slug: params.orgSlug }, status: "published" },
+    orderBy: { createdAt: "desc" },
+  });
+
+  return (
+    <div className="bg-slate-200">
+      <div className="mx-64 rounded-b-md bg-white px-16 pt-4">
+        <Title>Notícias</Title>
+        <ul role="list" className="divide-y divide-gray-100 py-4">
+          {news.map((n) => (
+            <li
+              key={n.id}
+              className="flex items-center justify-between gap-x-6 py-5"
+            >
+              <div className="min-w-0">
+                <div className="flex items-start gap-x-3">
+                  {n.imageUrl && (
+                    <img
+                      className="size-8 rounded-full"
+                      src={n.imageUrl}
+                      alt=""
+                    />
+                  )}
+                  <a
+                    href={`/noticias/${n.slug}`}
+                    className="text-sm font-semibold leading-6 text-gray-900 hover:underline"
+                  >
+                    {n.title}
+                  </a>
+                </div>
+                <div className="mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500">
+                  <p className="whitespace-nowrap">{n.subtitle}</p>
+                  <svg viewBox="0 0 2 2" className="h-0.5 w-0.5 fill-current">
+                    <circle cx={1} cy={1} r={1} />
+                  </svg>
+                  <div className="truncate">
+                    {<Date date={n.createdAt} format="DD/MM/YYYY" />}
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-none items-center gap-x-4">
+                <a
+                  href={`/noticias/${n.slug}`}
+                  className="hidden rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:block"
+                >
+                  Ver mais<span className="sr-only">, {n.title}</span>
+                </a>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
--- apps/dash/src/app/(frontend)/org/[orgSlug]/page.tsx
+++ apps/dash/src/app/(frontend)/org/[orgSlug]/page.tsx
@@ -61,49 +61,42 @@ export default async function CompanyHome({
 
   return (
     <>
-      <div className="bg-slate-200 lg:h-fit">
-        <div
-          className={clsx(
-            "min-h-[600px]  rounded-b-md bg-white px-4 pb-4 pt-4 xl:mx-16 2xl:mx-72"
-          )}
-        >
-          <div className="grid grid-cols-6 gap-6 ">
-            <div className={clsx("col-span-6 flex flex-col gap-4 ")}>
-              {organization.options?.images?.hero && (
-                <div className="relative h-[155px] w-auto lg:h-[375px]">
-                  <Image
-                    fill={true}
-                    className="rounded-md"
-                    src={organization.options?.images?.hero || ""}
-                    alt=""
-                  />
-                </div>
-              )}
-            </div>
-            <div className="col-span-6 mb-2">
-              <Heading>Próximos Eventos</Heading>
-            </div>
+      <div className="bg-slate-200 lg:h-[calc(100dvh-80px)]">
+        <div className={clsx("rounded-b-md bg-white pb-4 xl:mx-16 2xl:mx-72")}>
+          <div className="col-span-6 flex flex-col gap-4">
+            {organization.options?.images?.hero && (
+              <div className="relative w-auto ">
+                <img src={organization.options?.images?.hero || ""} alt="" />
+              </div>
+            )}
+          </div>
+          <div className="mt-4 grid grid-cols-6 gap-4 px-4">
+            <div className="col-span-6 md:col-span-2">
+              <div className="col-span-6 mb-2 ">
+                <Heading>Próximos Eventos</Heading>
+              </div>
 
-            <For
-              each={[...eventGroups, ...events]}
-              fallback={
-                <>
-                  <div className="mt-1 flex gap-2 text-sm text-gray-500">
-                    Mais em breve!{" "}
-                  </div>
-                </>
-              }
-            >
-              {(event) => {
-                return (
-                  <div className="col-span-6 flex flex-col md:col-span-2 ">
-                    <div className="flex flex-col gap-4 lg:grid">
-                      <EventCard event={event} orgSlug={params.orgSlug} />
+              <For
+                each={[...eventGroups, ...events]}
+                fallback={
+                  <>
+                    <div className="mt-1 flex gap-2 text-sm text-gray-500">
+                      Mais em breve!{" "}
                     </div>
-                  </div>
-                );
-              }}
-            </For>
+                  </>
+                }
+              >
+                {(event) => {
+                  return (
+                    <div className="col-span-6 flex flex-col md:col-span-2 ">
+                      <div className="flex flex-col gap-4 lg:grid">
+                        <EventCard event={event} orgSlug={params.orgSlug} />
+                      </div>
+                    </div>
+                  );
+                }}
+              </For>
+            </div>
 
             <div className="col-span-6 flex-col md:col-span-4 md:flex">
               <NewsCard news={news} />
