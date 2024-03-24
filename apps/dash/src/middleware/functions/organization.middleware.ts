d710a8f98d8118ee011aa265f611976dd734cee1
gcmarcello <marcellogcoelho@outlook.com>
2024-03-24 00:21:21 +0000

    Refactor organization middleware to include user role and permission

--- apps/dash/src/middleware/functions/organization.middleware.ts
+++ apps/dash/src/middleware/functions/organization.middleware.ts
@@ -2,22 +2,38 @@
 import { cookies } from "next/headers";
 import { prisma } from "prisma/prisma";
 import { UserSessionMiddlewareReturnType } from "./userSession.middleware";
+import { OrganizationPermission } from "@prisma/client";
 
 export async function OrganizationMiddleware<T>({
   request,
-}: UserSessionMiddlewareReturnType<T>) {
+  permission,
+}: UserSessionMiddlewareReturnType<T> & {
+  permission?: OrganizationPermission;
+}) {
   const activeOrg = cookies().get("activeOrg")?.value;
 
   if (!activeOrg) throw "Você não está em uma organização.";
 
   const organization = await prisma.organization.findFirst({
     where: {
-      ownerId: request.userSession.id,
       id: activeOrg,
+    },
+    include: {
+      OrganizationRole: { include: { OrganizationRolePermission: true } },
     },
   });
 
-  if (!organization)
+  const userRole = await prisma.user.findFirst({
+    where: { id: request.userSession.id },
+    select: {
+      OrganizationRole: {
+        where: { organizationId: activeOrg },
+        include: { OrganizationRolePermission: true },
+      },
+    },
+  });
+
+  if (organization?.ownerId !== request.userSession.id)
     throw "Você não tem permissão para acessar os dados dessa organização.";
 
   return {
