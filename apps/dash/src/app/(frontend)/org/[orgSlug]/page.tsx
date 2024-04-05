c336b7bb4f6f87c9f1d12a55136e5bb6f701a474
gcmarcello <marcellogcoelho@outlook.com>
2024-04-05 18:56:38 +0000

    Refactor EventCard and CompanyHome components

--- apps/dash/src/app/(frontend)/org/[orgSlug]/page.tsx
+++ apps/dash/src/app/(frontend)/org/[orgSlug]/page.tsx
@@ -61,51 +61,55 @@ export default async function CompanyHome({
 
   return (
     <>
-      <div className="bg-slate-200 lg:h-[calc(100dvh-80px)]">
-        <div className={clsx("rounded-b-md bg-white pb-4 xl:mx-16 2xl:mx-72")}>
-          <div className="col-span-6 flex  flex-col gap-4">
-            {organization.options?.images?.hero && (
-              <div className="relative w-auto ">
-                <img
-                  className="max-h-96 w-full"
-                  src={organization.options?.images?.hero || ""}
-                  alt=""
-                />
-              </div>
-            )}
-          </div>
-          <div className="mt-4 grid grid-cols-6 gap-4 px-4">
-            <div className="col-span-6 md:col-span-2">
-              <div className="col-span-6 mb-2 ">
-                <Heading>Próximos Eventos</Heading>
-              </div>
-
-              <div className="space-y-3">
-                <For
-                  each={[...eventGroups, ...events]}
-                  fallback={
-                    <>
-                      <div className="mt-1 flex gap-2 text-sm text-gray-500">
-                        Mais em breve!{" "}
-                      </div>
-                    </>
-                  }
-                >
-                  {(event) => {
-                    return (
-                      <div className="col-span-6 flex flex-col md:col-span-2">
-                        <div className="flex flex-col gap-4 lg:grid">
-                          <EventCard event={event} orgSlug={params.orgSlug} />
-                        </div>
-                      </div>
-                    );
-                  }}
-                </For>
-              </div>
+      <div className="bg-slate-200 ">
+        <div className="pb-20">
+          <div
+            className={clsx("rounded-b-md bg-white pb-4 xl:mx-16 2xl:mx-72")}
+          >
+            <div className="col-span-6 flex  flex-col gap-4">
+              {organization.options?.images?.hero && (
+                <div className="relative w-auto ">
+                  <img
+                    className="max-h-96 w-full"
+                    src={organization.options?.images?.hero || ""}
+                    alt=""
+                  />
+                </div>
+              )}
             </div>
+            <div className="mt-4 grid grid-cols-6 gap-4 px-4">
+              <div className="xxl:col-span-1 col-span-6 md:col-span-2">
+                <div className="col-span-6 mb-2 ">
+                  <Heading>Próximos Eventos</Heading>
+                </div>
 
-            <div className="col-span-6 flex-col md:col-span-4 md:flex">
-              <NewsCard news={news} />
+                <div className="space-y-3">
+                  <For
+                    each={[...eventGroups, ...events]}
+                    fallback={
+                      <>
+                        <div className="mt-1 flex gap-2 text-sm text-gray-500">
+                          Mais em breve!{" "}
+                        </div>
+                      </>
+                    }
+                  >
+                    {(event) => {
+                      return (
+                        <div className="col-span-6 flex flex-col md:col-span-2">
+                          <div className="flex flex-col gap-4 lg:grid">
+                            <EventCard event={event} orgSlug={params.orgSlug} />
+                          </div>
+                        </div>
+                      );
+                    }}
+                  </For>
+                </div>
+              </div>
+
+              <div className="xxl:col-span-5 col-span-6 flex-col md:col-span-4 md:flex">
+                <NewsCard news={news} />
+              </div>
             </div>
           </div>
         </div>
