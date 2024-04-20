946c15f03d0c95908dd6bcf69989f8398bfd4662
gcmarcello <marcellogcoelho@outlook.com>
2024-04-20 23:33:39 +0000

    Refactor NewsCard component to improve layout and readability

--- apps/dash/src/app/(frontend)/org/_shared/NewsCard.tsx
+++ apps/dash/src/app/(frontend)/org/_shared/NewsCard.tsx
@@ -29,26 +29,28 @@ export default function NewsCard({ news }: { news?: News[] }) {
         >
           {(item) => (
             <li className="px-4 py-2">
-              <div className="flex items-center gap-x-3">
+              <div className="flex items-center gap-x-3 lg:flex-row">
                 {item.imageUrl && (
                   <img
                     src={item.imageUrl || ""}
                     alt=""
-                    className="h-8 w-8 flex-none rounded-3xl bg-gray-800"
+                    className="h-20 max-w-32 flex-none rounded-lg "
                   />
                 )}
-                <Link href={`/noticias/${item.slug}`}>
-                  <h3 className="flex-auto text-sm font-semibold leading-6 text-gray-900">
-                    {item.title}
-                  </h3>
-                </Link>
+                <div>
+                  <Link href={`/noticias/${item.slug}`}>
+                    <h3 className="flex-auto text-sm font-semibold leading-6 text-gray-900">
+                      {item.title}
+                    </h3>
+                  </Link>
+                  <div className="mt-1 flex gap-2 text-sm text-gray-500">
+                    {item.subtitle}
+                  </div>
+                </div>
               </div>
               <time className="flex-none text-xs text-gray-500">
                 {date(item.updatedAt ?? item.createdAt, "DD/MM/YYYY")}
               </time>
-              <div className="mt-1 flex gap-2 text-sm text-gray-500">
-                {item.subtitle}
-              </div>
             </li>
           )}
         </For>
