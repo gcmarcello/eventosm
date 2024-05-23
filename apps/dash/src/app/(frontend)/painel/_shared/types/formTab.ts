a7b8a63dfc0264aa3ee2c0d4282748439b3bd8eb
gcmarcello <marcellogcoelho@outlook.com>
2024-05-23 20:04:25 +0000

    refactor: Update tab control naming and navigation in EventNavbar component

--- apps/dash/src/app/(frontend)/painel/_shared/types/formTab.ts
+++ apps/dash/src/app/(frontend)/painel/_shared/types/formTab.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
 
-export const formTab = z.object({ currentTab: z.string() });
+export const tabControl = z.object({ currentTab: z.string() });
 
-export type FormTab = z.infer<typeof formTab>;
+export type TabControl = z.infer<typeof tabControl>;
