71e10b320a965132e04ee61490198b9d8ab8bf44
gcmarcello <marcellogcoelho@outlook.com>
2024-05-27 22:11:13 +0000

    refactor: Remove unused code and update database schema

--- apps/dash/prisma/migrations/20240527220710_registration_details_and_absence_comment/migration.sql
+++ apps/dash/prisma/migrations/20240527220710_registration_details_and_absence_comment/migration.sql
@@ -1,19 +1,12 @@
 /*
   Warnings:
 
-  - You are about to drop the column `orderId` on the `EventRegistration` table. All the data in the column will be lost.
-  - You are about to drop the `Order` table. If the table is not empty, all the data it contains will be lost.
 
 */
--- DropForeignKey
-ALTER TABLE "public"."EventRegistration" DROP CONSTRAINT "EventRegistration_orderId_fkey";
 
 -- AlterTable
 ALTER TABLE "public"."EventAbsences" ADD COLUMN     "comment" TEXT;
 
 -- AlterTable
-ALTER TABLE "public"."EventRegistration" DROP COLUMN "orderId",
 ADD COLUMN     "additionalInfo" JSONB;
 
--- DropTable
-DROP TABLE "public"."Order";
