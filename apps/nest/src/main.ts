68cc9d0389ebe95f036263a866944671408cc2fe
gcmarcello <marcellogcoelho@outlook.com>
2024-05-27 14:59:54 +0000

    feat: increase json limit for all routes (TEMP)

--- apps/nest/src/main.ts
+++ apps/nest/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { json } from "express";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,7 @@ async function bootstrap() {
       whitelist: true,
     })
   );
+  app.use(json({ limit: "50mb" }));
   app.enableCors();
   await app.listen(5000);
 }
