2ab35a16cbc3873e78f56c4a13f66e8040c3da23
gcmarcello <marcellogcoelho@outlook.com>
2024-06-02 05:51:20 +0000

    feat: sidebar styling changes

--- apps/dash/src/app/(frontend)/painel/eventos/_shared/EventSidebar.tsx
+++ apps/dash/src/app/(frontend)/painel/eventos/_shared/EventSidebar.tsx
@@ -98,63 +98,22 @@ export function EventSidebar({ event }: { event: Event }) {
           </SidebarSection>
 
           <SidebarSpacer />
+        </SidebarBody>
+        <SidebarFooter className="max-lg:hidden">
           <SidebarSection>
             <SidebarItem href="/support">
               <QuestionMarkCircleIcon />
               <SidebarLabel>Suporte</SidebarLabel>
             </SidebarItem>
+            <SidebarItem href="/changelog">
+              <SparklesIcon />
+              <SidebarLabel>Changelog</SidebarLabel>
+            </SidebarItem>
           </SidebarSection>
-        </SidebarBody>
-        <SidebarFooter className="max-lg:hidden">
-          <Dropdown>
-            <DropdownButton as={SidebarItem}>
-              <span className="flex min-w-0 items-center gap-3">
-                <Avatar
-                  src="/profile-photo.jpg"
-                  className="size-10"
-                  square
-                  alt=""
-                />
-                <span className="min-w-0">
-                  <span className="block truncate text-sm/5 font-medium text-zinc-950 dark:text-white">
-                    Erica
-                  </span>
-                  <span className="block truncate text-xs/5 font-normal text-zinc-500 dark:text-zinc-400">
-                    erica@example.com
-                  </span>
-                </span>
-              </span>
-              <ChevronUpIcon />
-            </DropdownButton>
-            <DropdownMenu className="min-w-64" anchor="top start">
-              <DropdownItem href="/my-profile">
-                <UserIcon />
-                <DropdownLabel>My profile</DropdownLabel>
-              </DropdownItem>
-              <DropdownItem href="/settings">
-                <Cog8ToothIcon />
-                <DropdownLabel>Settings</DropdownLabel>
-              </DropdownItem>
-              <DropdownDivider />
-              <DropdownItem href="/privacy-policy">
-                <ShieldCheckIcon />
-                <DropdownLabel>Privacy policy</DropdownLabel>
-              </DropdownItem>
-              <DropdownItem href="/share-feedback">
-                <LightBulbIcon />
-                <DropdownLabel>Share feedback</DropdownLabel>
-              </DropdownItem>
-              <DropdownDivider />
-              <DropdownItem href="/logout">
-                <ArrowRightStartOnRectangleIcon />
-                <DropdownLabel>Sign out</DropdownLabel>
-              </DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
         </SidebarFooter>
       </Sidebar>
     ),
-    []
+    [pathname]
   );
   return (
     <>
