9e516e42de36136dc65241443b918025db7b4ca8
gcmarcello <marcellogcoelho@outlook.com>
2024-06-18 13:22:25 +0000

    refactor: Move organization permissions to separate entity file

--- apps/nest/src/resources/organizations/permissions.decorator.ts
+++ apps/nest/src/resources/organizations/permissions.decorator.ts
@@ -1,23 +1,5 @@
 import { Reflector } from "@nestjs/core";
-
-export enum OrganizationPermissions {
-  CreateOrganization = "create:organization",
-  UpdateOrganization = "update:organization",
-  DeleteOrganization = "delete:organization",
-  ReadPrivateOrganizations = "read:private-organizations",
-  CreateEvent = "create:event",
-  UpdateEvent = "update:event",
-  DeleteEvent = "delete:event",
-  ReadPrivateEvents = "read:private-events",
-  CreateTournament = "create:tournament",
-  UpdateTournament = "update:tournament",
-  DeleteTournament = "delete:tournament",
-  ReadPrivateTournaments = "read:private-tournaments",
-  CreateNews = "create:news",
-  UpdateNews = "update:news",
-  DeleteNews = "delete:news",
-  ReadPrivateNews = "read:private-news",
-}
+import { OrganizationPermissions } from "./entities/organizationPermission.entity";
 
 export const Permissions =
   Reflector.createDecorator<OrganizationPermissions[]>();
